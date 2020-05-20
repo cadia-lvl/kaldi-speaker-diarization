d2d355409a9e133013cfadf84c463ed804830d46
judyfong <judyfong@users.noreply.github.com>
2020-05-20 12:39:32 +0000

    v2 - Add dev set and step to extract i-vectors

--- v2/run.sh
+++ v2/run.sh
@@ -9,6 +9,7 @@
 # See ../README.txt for more info on data required.
 # Results (diarization error rate) are inline in comments below.
 #SBATCH --output=logs/run_%J.out
+#SBATCH --nodelist=terra
 #SBATCH -c 5
 
 . ./cmd.sh
@@ -23,7 +24,7 @@ ivector_dim=400
 ivec_dir=exp/extractor_c${num_components}_i${ivector_dim}
 
 
-stage=4
+stage=0
 
 if [ $stage -le 0 ]; then
   # Now prepare the VoxCeleb1 train and test data.  If you downloaded the corpus soon
@@ -31,13 +32,15 @@ if [ $stage -le 0 ]; then
   # can be invoked as follows:
   # local/make_voxceleb1.pl $voxceleb1_root data
   local/make_voxceleb1_v2.pl $voxceleb1_root dev data/voxceleb1_train
+  local/make_voxceleb1_v2.pl $voxceleb1_root test data/voxceleb1_test
 
   utils/combine_data.sh data/train data/voxceleb1_train
+  utils/combine_data.sh data/dev data/voxceleb1_test
 fi
 
 if [ $stage -le 1 ]; then
   # Make MFCCs for each dataset
-  for name in train; do
+  for name in train dev; do
     steps/make_mfcc.sh --write-utt2num-frames true \
       --mfcc-config conf/mfcc.conf --nj 20 --cmd "$train_cmd --max-jobs-run 20" \
       data/${name} exp/make_mfcc $mfccdir
@@ -46,17 +49,19 @@ if [ $stage -le 1 ]; then
 fi
 if [ $stage -le 2 ]; then
 
-  # Compute the energy-based VAD for train
-  sid/compute_vad_decision.sh --nj 20 --cmd "$train_cmd" \
-    data/train exp/make_vad $vaddir
-  utils/fix_data_dir.sh data/train
+  # Compute the energy-based VAD
+  for name in train dev; do
+    sid/compute_vad_decision.sh --nj 20 --cmd "$train_cmd" \
+      data/$name exp/make_vad $vaddir
+    utils/fix_data_dir.sh data/$name
+  done
 
   # This writes features to disk after adding deltas and applying the sliding window CMN.
   # Although this is somewhat wasteful in terms of disk space, for diarization
   # it ends up being preferable to performing the CMN in memory.  If the CMN
   # were performed in memory it would need to be performed after the subsegmentation,
   # which leads to poorer results.
-  for name in train; do
+  for name in train dev; do
     local/prepare_feats.sh --nj 20 --cmd "$train_cmd" \
       data/$name data/${name}_cmn exp/${name}_cmn
     if [ -f data/$name/vad.scp ]; then
@@ -73,11 +78,13 @@ if [ $stage -le 2 ]; then
   # The segments are created using an energy-based speech activity
   # detection (SAD) system, but this is not necessary.  You can replace
   # this with segments computed from your favorite SAD.
-  diarization/vad_to_segments.sh --nj 20 --cmd "$train_cmd" \
-      data/train_cmn data/train_cmn_segmented
+  for name in train dev; do
+    diarization/vad_to_segments.sh --nj 20 --cmd "$train_cmd" \
+        data/${name}_cmn data/${name}_cmn_segmented
+  done
 fi
 if [ $stage -le 3 ]; then
-  # Train the UBM on VoxCeleb 1
+  # Train the UBM on VoxCeleb 1 train data
   # UBM - Universal Background Model
   sid/train_diag_ubm.sh --cmd "$train_cmd --mem 4G" \
     --nj 40 --num-threads 8 \
@@ -110,3 +117,34 @@ if [ $stage -le 4 ]; then
     exp/full_ubm/final.ubm data/train_5k \
     $ivec_dir
 fi
+
+if [ $stage -le 5 ]; then
+  # Fix and validate the subset
+  utils/fix_data_dir.sh data/dev_cmn_segmented
+  utils/validate_data_dir.sh --no-text data/dev_cmn_segmented
+
+  # TODO Need to Extract i-vectors for evaluation set.
+  # Extract i-vectors for Voxceleb 1 development set.
+  # We set apply-cmn false and apply-deltas false because we already add
+  # deltas and apply cmn in stage 1.
+  diarization/extract_ivectors.sh --cmd "$train_cmd --mem 20G" \
+    --nj 40 --window 1.5 --period 0.75 --apply-cmn false --apply-deltas false \
+    --min-segment 0.5 $ivec_dir \
+    data/dev_cmn_segmented $ivec_dir/ivectors_dev_segmented
+
+  # Reduce the amount of training data for the PLDA training.
+  utils/subset_data_dir.sh data/train_cmn_segmented 6400 data/train_cmn_segmented_6400
+
+  # Fix and validate the subset
+  utils/fix_data_dir.sh data/train_cmn_segmented_6400
+  utils/validate_data_dir.sh --no-text data/train_cmn_segmented_6400
+
+  # Extract i-vectors for VoxCeleb 1 train, which is our PLDA training
+  # data.  A long period is used here so that we don't compute too
+  # many i-vectors for each recording.
+  diarization/extract_ivectors.sh --cmd "$train_cmd --mem 25G" \
+    --nj 40 --window 3.0 --period 10.0 --min-segment 1.5 --apply-cmn false --apply-deltas false \
+    --hard-min true $ivec_dir \
+    data/train_cmn_segmented_6400 $ivec_dir/ivectors_train_segmented_6400
+fi
+
